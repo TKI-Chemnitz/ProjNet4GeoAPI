72b5048f548465c9b6fb0256da49ed2aa1da0369
Mario Müller <m.mueller@tki-chemnitz.de>
2025-07-24 17:25:06 +0200

    Added support for Lambert Tangential Conformal Conic Projection

--- src/ProjNet/CoordinateSystems/Projections/ProjectionsRegistry.cs
+++ src/ProjNet/CoordinateSystems/Projections/ProjectionsRegistry.cs
@@ -38,8 +38,9 @@ namespace ProjNet.CoordinateSystems.Projections
 			Register("polyconic", typeof(PolyconicProjection));
 			
             Register("lambert_conformal_conic", typeof(LambertConformalConic2SP));
-			Register("lambert_conformal_conic_2sp", typeof(LambertConformalConic2SP));
-			Register("lambert_conic_conformal_(2sp)", typeof(LambertConformalConic2SP));
+            Register("lambert_conformal_conic_2sp", typeof(LambertConformalConic2SP));
+            Register("lambert_conic_conformal_(2sp)", typeof(LambertConformalConic2SP));
+            Register("lambert_tangential_conformal_conic_projection", typeof(LambertConformalConic2SP));
 
             Register("lambert_azimuthal_equal_area", typeof(LambertAzimuthalEqualAreaProjection));
 
